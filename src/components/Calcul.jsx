529c679d9f9f12f97e82521bfe85bbb3653c08b6
lamnaourm <mohammed.lamnaour@gmail.com>
2022-10-04 11:21:16 +0100

    first comit

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -4,24 +4,17 @@ class Calcul extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      capital: 0,
-      taux: 0,
-      duree: 0,
-      mensualite: 0,
+
     };
   }
   handleOnchange = (e) => {
-
     this.setState({[e.target.name] : e.target.value})
-
   };
 
   handleOnSubmit = (e) => {
     e.preventDefault();
 
-    const m = (this.state.capital * this.state.taux/100/12)/(1- Math.pow(1+this.state.taux/100/12,-this.state.duree));
-
-    this.setState({mensualite: m});
+    
   };
 
   render() {
@@ -29,22 +22,23 @@ class Calcul extends Component {
       <div className="calcul">
         <form onSubmit={this.handleOnSubmit}>
           <div>
-            <label>Capital empreunté : </label>
-            <input type="number" name="capital" value={this.state.capital} onChange={this.handleOnchange} />
+            <label>Distance : </label>
+            <input type="number" name="distance" />
           </div>
           <div>
-            <label>taux d'interet : </label>
-            <input type="number" name="taux" value={this.state.taux} onChange={this.handleOnchange}/>
+            <label>Poids : </label>
+            <input type="number" name="taux" />
           </div>
           <div>
-            <label>Duree rembourssement : </label>
-            <input type="number" name="duree" value={this.state.duree} onChange={this.handleOnchange}/>
+            <label>Mode de transport : </label>
+            <input type="radio" name="mode" /> Express 
+            <input type="radio" name="mode" /> Normale 
           </div>
           <hr />
           <input type="submit" value="Calculer" />
           <div>
-            <label>Mensualite : </label>
-            <input type="number" name="mensualite" value={this.state.mensualite.toFixed(2)} disabled/>
+            <label>Cout total : </label>
+            <input type="number" name="cout" />
           </div>
         </form>
       </div>

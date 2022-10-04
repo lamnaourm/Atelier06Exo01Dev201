0657b3959d1e6c3e07be82aa257cbb01eadca134
lamnaourm <mohammed.lamnaour@gmail.com>
2022-10-04 10:25:53 +0100

    2 commit

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -1,48 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Calcul extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      capital: 0,
+      taux: 0,
+      duree: 0,
+      mensualite: 0,
+    };
+  }
+  handleOnchange = (e) => {
 
-    constructor(props){
-        super(props);
-        this.state = {
+    this.setState({[e.target.name] : e.target.value})
 
-        }
-    }
-    handleOnchange = (e) => {
+  };
 
-    }
+  handleOnSubmit = (e) => {
+    e.preventDefault();
 
-    handleOnSubmit = (e) => {
-        e.preventDefault();
+    const m = (this.state.capital * this.state.taux/100/12)/(1- Math.pow(1+this.state.taux/100/12,-this.state.duree));
 
-    }
+    this.setState({mensualite: m});
+  };
 
-    render() {
-        return (
-            <div className='calcul'>
-                <form>
-                    <div>
-                        <label>Capital empreunté : </label>
-                        <input type='number' name='capital'/>
-                    </div>
-                    <div>
-                        <label>taux d'interet : </label>
-                        <input type='number' name='taux'/>
-                    </div>
-                    <div>
-                        <label>Duree rembourssement : </label>
-                        <input type='number' name='duree'/>
-                    </div>
-                    <hr/>
-                    <input type='submit' value='Calculer' />
-                    <div>
-                        <label>Mensualite : </label>
-                        <input type='number' name='mensualite'/>
-                    </div>
-                </form>    
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="calcul">
+        <form onSubmit={this.handleOnSubmit}>
+          <div>
+            <label>Capital empreunté : </label>
+            <input type="number" name="capital" value={this.state.capital} onChange={this.handleOnchange} />
+          </div>
+          <div>
+            <label>taux d'interet : </label>
+            <input type="number" name="taux" value={this.state.taux} onChange={this.handleOnchange}/>
+          </div>
+          <div>
+            <label>Duree rembourssement : </label>
+            <input type="number" name="duree" value={this.state.duree} onChange={this.handleOnchange}/>
+          </div>
+          <hr />
+          <input type="submit" value="Calculer" />
+          <div>
+            <label>Mensualite : </label>
+            <input type="number" name="mensualite" value={this.state.mensualite.toFixed(2)} disabled/>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Calcul;
